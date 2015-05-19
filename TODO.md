6bf3853f3d987330a7eced4030d83917d6111054
badview08 <jonesgabriel21@gmail.com>
2015-05-19 13:44:37 -0400

    copied files to codepen for further viewing

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
 when I click start.
--Display the time elapsed since the button click
+-Display the time elapsed since the button click every second
 -change the style of the button to be blue
 -Change the txt of the button to be pause
--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 (function() {
 	'use strict';
 	var startTime;
+	var $hours = document.querySelector('.elapsed-time .hours');
+	var $minutes = document.querySelector('.elapsed-time .minutes');
+	var $seconds = document.querySelector('.elapsed-time .seconds');
 	
 	function startClicked(event){
 		startTime = new Date();
@@ -9,7 +12,35 @@
 	}
 	function logElapsedTime() {
 		var currentTime = new Date();
-		console.log(currentTime - startTime);
+		var interval = currentTime - startTime;
+		var elapsed = millisecondsToInterval(interval);
+		displayInterval(elapsed);
+	}
+	
+	function displayInterval(interval) {
+		$seconds.textcontent = interval[2];
+	}
+	function formatInterval(interval) {
+		var formatted = [];
+		formatted[0] = ("0" + interval[0]);
+	}
+	
+	function millisecondsToInterval(ms) {
+		var msLeft = ms;
+		
+		var msInHour = 3600000;
+		var msInMin = 60000;
+		var msInSec = 1000;
+		
+		var hours = Math.floor(msLeft / msInHour);
+		msLeft = msLeft % msInHour;
+		
+		var minutes = Math.floor(msLeft / msInMin);
+		msLeft = msLeft % msInMin;
+		var seconds = Math.floor(msLeft / msInSec);
+		msLeft = msLeft % msInSec;
+		
+		return [hours, minutes, seconds];
 	}
 	document.querySelector('.start').addEventListener('click', startClicked);
 })();
